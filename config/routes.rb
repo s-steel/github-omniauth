649857f90dc96021cd8ad8a96fe6993fb2e09fdb
Sean Steel <seanrsteel@gmail.com>
2020-12-28 11:08:29 -0500

    Add logout link and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
   get '/auth/github/callback', to: 'sessions#create'
-  # get '/logout', to: 'sessions#destroy' as: :logout
+  get '/logout', to: 'sessions#destroy'
 end
